5a184477039257f7df223f8d8379c7f12eea4be9
Kubbany <2021170289@cis.asu.edu.eg>
2025-07-09 15:02:49 +0300

    add auth repo and create user entity

---
+++ lib/Features/authentication/domain/entites/user_entity.dart
@@ -0,0 +1,5 @@
+class UserEntity {
+  final String uId, email;
+
+  const UserEntity({required this.uId, required this.email});
+}
---
+++ lib/Features/authentication/domain/repositories/auth_repo.dart
@@ -0,0 +1,7 @@
+import 'package:booki/Features/authentication/domain/entites/user_entity.dart';
+import 'package:booki/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email, String password);
+}
